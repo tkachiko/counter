1ac3f7983f0cecbc5838af634692bc416c35f880
tkachiko <tkachikos@gmail.com>
2022-11-01 21:59:35 +0300

    refactor Display module

--- src/components/Counter/Display/Display.tsx
+++ src/components/Counter/Display/Display.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import s from './Display.module.css';
+import {Button} from '../../Button/Button';
 
 export type DisplayPropType = {
   maxValue: number
   count: number
+  startValue: number
+  reset: () => void
+  increment: () => void
 }
 
 export const Display = (props: DisplayPropType) => {
-  const {count, maxValue} = props;
+  const {count, maxValue, startValue, reset, increment} = props;
+
+  const isIncButtonDisabled = count === maxValue
+    // || startValue >= maxValue;
+
+  // const isResetButtonDisabled = count === startValue || startValue > maxValue;
+
+  const incBtn = <span>inc</span>;
+  const resBtn = <span>reset</span>;
 
   const finalClassName = s.default
-    + ' ' + (count === maxValue
+    + ' ' + (isIncButtonDisabled
       ? s.red
       : s.default);
 
@@ -18,6 +30,11 @@ export const Display = (props: DisplayPropType) => {
     <div className={s.display}>
       <div className={finalClassName}>
         {count}
+      </div>
+
+      <div className={s.buttons}>
+        <Button disabled={isIncButtonDisabled} callBack={increment}>{incBtn}</Button>
+        <Button disabled={false} callBack={reset}>{resBtn}</Button>
       </div>
     </div>
   );
